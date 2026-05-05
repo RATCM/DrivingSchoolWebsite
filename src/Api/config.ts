85a82d84fcdc6bf73b7404e86ee736c76039427f
mikkelthe <s204107@student.dtu.dk>
2026-05-05 16:37:23 +0200

    Containerisation

--- src/Api/config.ts
+++ src/Api/config.ts
@@ -1,5 +1,5 @@
 export const API_BASE_URL =
-    import.meta.env.VITE_API_BASE_URL ?? "http://localhost:5259/";
+    process.env.REACT_APP_API_BASE_URL
 
 export const GOOGLE_MAPS_API_KEY =
-    import.meta.env.VITE_GOOGLE_MAPS_API_KEY ?? "";
+    process.env.REACT_APP_GOOGLE_MAPS_API_KEY
