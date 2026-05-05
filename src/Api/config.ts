fe9ce60886f740d98ba06cc761efae827361d2a5
mikkelthe <s204107@student.dtu.dk>
2026-05-05 14:03:03 +0200

    Things for Google Maps API

--- src/Api/config.ts
+++ src/Api/config.ts
@@ -1,2 +1,5 @@
-export const API_BASE_URL = process.env.REACT_APP_API_URL!;
-export const GOOGLE_MAPS_API_KEY = process.env.REACT_APP_GOOGLE_MAPS_API_KEY!;
+export const API_BASE_URL =
+    import.meta.env.VITE_API_BASE_URL ?? "http://localhost:5259/";
+
+export const GOOGLE_MAPS_API_KEY =
+    import.meta.env.VITE_GOOGLE_MAPS_API_KEY ?? "";
